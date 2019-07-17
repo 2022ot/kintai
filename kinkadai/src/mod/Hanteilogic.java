e7933b4203665bacb23135d7f4d1cc68166d8d48
Ao-ito0 <Ao-ito0@AOPC05>
2019-07-17 18:56:02 +0900

    ちょい変更

--- kinkadai/src/mod/Hanteilogic.java
+++ kinkadai/src/mod/Hanteilogic.java
@@ -1,18 +1,23 @@
 package mod;
 
+
 public class Hanteilogic {
 	public boolean execute(Hantei hantei) {//booleanからvoidへ変更
 
 		String syu="syu";
 		String ki="ki";
+		hantei
+
 
 		if(syu.equals(syu)) {
-
-
-		}
+			hantei="syu";//ここをなんとかしたい。
+			}else {
+				hantei="ki";
+			}
 		/**HanteiDAO dao = new HanteiDAO();
 		Handan handan = dao.saveToDb(handan);
 		return handan!=null;*/
 		return false;
+
 	}
 }
